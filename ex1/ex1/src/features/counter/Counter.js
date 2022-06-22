834233ec1a03056abc5c8753d4f7d98bb7c25eb2
newasad <asadhmeed2@gmail.com>
2022-06-22 19:51:32 +0300

    finish lesson 1

--- ex1/ex1/src/features/counter/Counter.js
+++ ex1/ex1/src/features/counter/Counter.js
@@ -1,18 +1,36 @@
 
+import { useState ,useRef} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {increment , decrement} from './counterSlice';
+import {increment , decrement ,reset , incrementByAmount} from './counterSlice';
 
 
 const Counter = () => {
     const count = useSelector((state)=> state.counter.count);
     const despatch = useDispatch();
+    const [amount,setAmount] =useState(0);
+    const amountRef = useRef();
+    
+
+    let addValue = parseInt(amount || 0 );
+
+    const resetAll = ()=>{
+        amountRef.current.value =0;
+        despatch(reset());
+    }
+
+    const amountHandler =(e)=>{
+        setAmount(parseInt(e.target.value)) 
+    }
 
   return (
     <section>
         <p>{count}</p>
+        <input type="number" id="inc-amount" ref={amountRef} onChange={amountHandler}/>
         <div>
-            <button onClick = {()=>{despatch(increment())}}>+</button>
+            <button onClick = {()=>{despatch(increment())}}>+</button> 
             <button onClick = {()=>{despatch(decrement())}}>-</button>
+            <button onClick = {()=>{resetAll()}}>Reset</button> 
+            <button onClick = {()=>{despatch(incrementByAmount(addValue))}}>+n</button> 
         </div>
     </section>
     
