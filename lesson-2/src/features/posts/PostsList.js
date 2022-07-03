3680e3ac1d3c689bc76fb63ffe585bdcc40c56f0
newasad <asadhmeed2@gmail.com>
2022-07-03 16:32:39 +0300

    finish of lesson 4

--- lesson-2/src/features/posts/PostsList.js
+++ lesson-2/src/features/posts/PostsList.js
@@ -1,21 +1,16 @@
-import { useEffect } from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from './postsSlice';
+
+import { useSelector } from 'react-redux';
+import { selectAllPosts, getPostsStatus, getPostsError } from './postsSlice';
 
 import PostsExcerpt from "./postsExcerpt"
 
 
 const PostsList= ()=>{
-    const dispatch = useDispatch()
+
     const posts = useSelector(selectAllPosts)
     const postsStatus = useSelector(getPostsStatus)
     const error = useSelector(getPostsError)
-
-    useEffect(()=>{
-        if(postsStatus === 'idle'){
-            dispatch(fetchPosts())}
-    },[postsStatus, dispatch])
     
     let content;
     if(postsStatus === 'loading'){
