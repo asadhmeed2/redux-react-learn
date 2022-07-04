0e56250abd48b9adb403cfb09765513f3bdd97ad
newasad <asadhmeed2@gmail.com>
2022-07-04 22:57:08 +0300

    count-button

--- lesson-2/src/components/Header.js
+++ lesson-2/src/components/Header.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
-import './header.style.css'
+import './header.style.css';
+import { useSelector, useDispatch } from "react-redux";
+import { getCount,increaseCount } from '../features/posts/postsSlice'
 
 const Header = ( )=>{
+    const count =useSelector(getCount);
+    const dispatch = useDispatch();
     return (
     <header className="nav-container">
         <h1 className="logo">Redux Blog</h1>
@@ -9,6 +13,7 @@ const Header = ( )=>{
             <li><Link to="/" className="nav-link">Home</Link></li>
             <li><Link to="post" className="nav-link">Posts</Link></li>
             <li><Link to="user" className="nav-link">Users</Link></li>
+            <button type='botton' class='count-button' onClick={()=>dispatch(increaseCount())}>{count}</button>
         </nav>
     </header>
     )
