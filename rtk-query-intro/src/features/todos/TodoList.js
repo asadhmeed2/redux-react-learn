2ba51ba5c2f5a6c07565ae0fd6c0324753539631
newasad <asadhmeed2@gmail.com>
2022-07-07 21:21:20 +0300

    get the data to todo list component

--- rtk-query-intro/src/features/todos/TodoList.js
+++ rtk-query-intro/src/features/todos/TodoList.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUpload } from '@fortawesome/free-solid-svg-icons'
+import { useGetTodosQuery } from '../api/apiSlice';
 
 const TodoList = () => {
 
     const [newTodo,setNewTodo] =useState('');
+
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }=useGetTodosQuery();
+
 
     const handleSubmit = (e)=>{
     e.preventDefault();
     //add todo to state
     setNewTodo('');
 }
+
 
     const newItemSection = (<form onSubmit ={handleSubmit}>
         <label htmlFor='new-todo'>Enter a new todo item`</label>
@@ -27,9 +38,21 @@ const TodoList = () => {
             <FontAwesomeIcon icon={faUpload}/>
         </button>
   </form>)
+
+  let content;
+  if(isLoading){
+    content = <p>Loading...</p>
+  }else if(isSuccess){
+    content = JSON.stringify(todos)//replace with the jsx to render the todo list items
+  }else if (isError){
+    content =<p>{error}</p>
+  }
+
   return (
-    {newItemSection}
-    //show all todos 
+    <main>
+        {newItemSection}
+        {content}
+    </main>
   )
 }
 
