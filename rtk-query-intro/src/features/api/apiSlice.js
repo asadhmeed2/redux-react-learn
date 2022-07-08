7a9e911417fba835cad56e94bd3c93e1fa4967eb
newasad <asadhmeed2@gmail.com>
2022-07-08 17:05:25 +0300

    add providesTags to get the data when we add remove update the data in the db

--- rtk-query-intro/src/features/api/apiSlice.js
+++ rtk-query-intro/src/features/api/apiSlice.js
@@ -3,30 +3,35 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice =createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
+    tagTypes: ['Todos'],//to get the data on change
     endpoints:(builder)=>({ 
         getTodos: builder.query({
         query:()=>'/todos',
+        providesTags:['Todos']
         }),
         addTodo: builder.mutation({
             query :(todo)=>({
                 url:'/todos',
                 method: 'POST',
                 body:todo,
-            })
+            }),
+            invalidatesTags:['Todos']//to get the data on change
         }),
         updateTodo: builder.mutation({
             query :(todo)=>({
                 url:`/todos/${todo.id}`,
                 method: 'PATCH',
                 body: todo
-            })
+            }),
+            invalidatesTags:['Todos']//to get the data on change
         }),
         deleteTodo: builder.mutation({
             query: ({id})=>({
                 url: `todos/${id}`,
                 method: 'DELETE',
                 body: id
-            })
+            }),
+            invalidatesTags:['Todos']//to get the data on change
         })
     }), 
 
