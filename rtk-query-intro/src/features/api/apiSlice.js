a6bb18dedd451e413894f3de3acc052a9aa510a6
newasad <asadhmeed2@gmail.com>
2022-07-08 14:51:41 +0300

    add new endPoint to post a todo item

--- rtk-query-intro/src/features/api/apiSlice.js
+++ rtk-query-intro/src/features/api/apiSlice.js
@@ -3,9 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice =createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500'}),
-    endpoints:(builder)=>({ getTodos: builder.query({
+    endpoints:(builder)=>({ 
+        getTodos: builder.query({
         query:()=>'/todos',
-    })})
+        }),
+        addTodo: builder.mutation({
+            query :(todo)=>({
+                url:'/todos',
+                method: 'POST',
+                body:todo,
+            })
+        })
+    }), 
+
 })
 
 //custom hooks from apiSlice
