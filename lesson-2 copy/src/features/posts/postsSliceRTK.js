f9d55e3b3ad92107e560e522ae2072d38c010769
newasad <asadhmeed2@gmail.com>
2022-07-21 15:09:56 +0300

    add addpost updatePost fun

--- lesson-2 copy/src/features/posts/postsSliceRTK.js
+++ lesson-2 copy/src/features/posts/postsSliceRTK.js
@@ -68,6 +68,39 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
                     ...result.ids.map(id=>({ type:'Post', id }))
                 ]
             }
+        }),
+        addNewPost: builder.mutation({
+            query: initialPost =>({ 
+                url: '/posts',
+                method: 'POST',
+                body:{
+                    ...initialPost,
+                    userId: Number(initialPost.uerId),
+                    reactions:{
+                        thumbsUp:0,
+                        wow:0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0,
+                    }
+                }
+            }),
+            invalidatesTags: [
+                {type: 'Post',id:'LIST'}
+            ]
+        }),
+        updatePost: builder.mutation({ 
+            query: initialPost =>({ 
+                url:`/posts/${initialPost.id}`,
+                method: 'PUT',
+                body:{
+                    ...initialPost,
+                    date:new Date().toISOString()
+                }
+            }),
+            invalidatesTags: (result, error,arg)=>[
+                { type: 'Post', id: arg.id}
+            ]
         })
     })
 })
